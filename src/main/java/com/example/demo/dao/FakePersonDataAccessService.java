365021e24d94cef23a63882a3928ab9250688022
devuu1395 <48145422+devuu1395@users.noreply.github.com>
2020-07-10 00:48:34 +0530

    Routes integrated

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -16,6 +17,44 @@ public class FakePersonDataAccessService implements PersonDao {
     @Override
     public int insertPerson(UUID id, Person person) {
         Db.add(new Person(id,person.getName()));
+        return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return Db;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return Db.stream()
+                .filter(person ->  person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        Optional<Person> personMaybe=selectPersonById(id);
+        if (!personMaybe.isPresent()) {
+            return 0;
+        }
+        Db.remove(personMaybe.get());
         return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person update) {
+        return selectPersonById(id)
+                .map(person->{
+                    int indexOfPersonToUpdate=Db.indexOf(person);
+                    if(indexOfPersonToUpdate>=0)
+                    {
+                        Db.set(indexOfPersonToUpdate,new Person(id,update.getName()));
+                        return 1;
+                    }
+                    return 0;
+                })
+                .orElse(0);
+
     }
 }
